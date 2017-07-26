2221decc6ef81893f6cc3ae10f057ec999430392
Joe Maples <joe@frap129.org>
2017-07-25 19:39:48 -0500

    adreno_idler: Conver to State Notifier
    Signed-off-by: Joe Maples <joe@frap129.org>

--- drivers/devfreq/adreno_idler.c
+++ drivers/devfreq/adreno_idler.c
@@ -29,6 +29,7 @@
 
 #include <linux/module.h>
 #include <linux/devfreq.h>
+#include <linux/state_notifier.h>
 #include <linux/msm_adreno_devfreq.h>
 
 #define ADRENO_IDLER_MAJOR_VERSION 1
@@ -81,6 +82,10 @@ int adreno_idler(struct devfreq_dev_status stats, struct devfreq *devfreq,
 			idlecount--;
 			return 1;
 		}
+	} else if (state_suspended) {
+		/* GPU shouldn't be used for much while display is off, so ramp down the frequency */
+		*freq = devfreq->profile->freq_table[devfreq->profile->max_state - 1];
+		return 1;
 	} else {
 		idlecount = 0;
 		/* Do not return 1 here and allow rest of the algorithm to
