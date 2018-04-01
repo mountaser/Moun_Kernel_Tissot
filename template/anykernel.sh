d561b1ea417afab7075474033d473f3af563d29e
Mountaser Halak <mountaser1998@gmail.com>
2018-04-01 02:15:28 +0200

    Typo xD

--- template/anykernel.sh
+++ template/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() {
-kernel.string=MiNTKernel by GamingDevisCC @ xda-developers
+kernel.string=Moun Kernel by Mountaser Halak @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
