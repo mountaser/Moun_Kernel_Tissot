05113f5f94e9c34feaef971fae40a5c1f09b4eb3
Mountaser Halak <mountaser1998@gmail.com>
2018-05-16 19:26:47 +0200

    cpufreq: fixed a build error by removing a duplicated line

--- drivers/cpufreq/cpufreq_governor.h
+++ drivers/cpufreq/cpufreq_governor.h
@@ -185,8 +185,7 @@ struct cs_dbs_tuners {
 	unsigned int sampling_down_factor;
 	unsigned int up_threshold;
 	unsigned int down_threshold;
-	unsigned int freq_step;
-    	unsigned int down_threshold_suspended;
+	unsigned int down_threshold_suspended;
 	unsigned int freq_step;
 	unsigned int sleep_depth;
  	unsigned int boost_enabled;
